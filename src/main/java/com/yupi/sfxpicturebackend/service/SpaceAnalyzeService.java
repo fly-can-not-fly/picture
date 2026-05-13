04dcff33a82ae09b457379d6506de50c55987e1c
FLY <2904380845@qq.com>
2026-05-13 16:56:09 +0800

    个人可以对自己的空间进行分析，管理员可以对所有的空间进行分析。

---
+++ src/main/java/com/yupi/sfxpicturebackend/service/SpaceAnalyzeService.java
@@ -0,0 +1,29 @@
+package com.yupi.sfxpicturebackend.service;
+
+import com.yupi.sfxpicturebackend.model.dto.space.analyze.*;
+import com.yupi.sfxpicturebackend.model.entity.Space;
+import com.yupi.sfxpicturebackend.model.entity.User;
+import com.yupi.sfxpicturebackend.model.vo.space.*;
+
+import java.util.List;
+
+/**
+ * @author 孙飞翔
+ */
+public interface SpaceAnalyzeService {
+    SpaceUsageAnalyzeResponse getSpaceUsageAnalyze(SpaceUsageAnalyzeRequest spaceUsageAnalyzeRequest, User loginUser);
+
+    List<SpaceCategoryAnalyzeResponse> getSpaceCategoryAnalyze(SpaceCategoryAnalyzeRequest spaceCategoryAnalyzeRequest, User loginUser);
+
+    List<SpaceTagAnalyzeResponse> getSpaceTagAnalyze(SpaceTagAnalyzeRequest spaceTagAnalyzeRequest, User loginUser);
+
+    List<SpaceSizeAnalyzeResponse> getSpaceSizeAnalyze(SpaceSizeAnalyzeRequest spaceSizeAnalyzeRequest, User loginUser);
+
+    List<SpaceUserAnalyzeResponse> getSpaceUserAnalyze(SpaceUserAnalyzeRequest spaceUserAnalyzeRequest, User loginUser);
+
+    List<Space> getSpaceRankAnalyze(SpaceRankAnalyzeRequest spaceRankAnalyzeRequest, User loginUser);
+
+    void checkSpaceAnalyzeAuth(SpaceAnalyzeRequest spaceAnalyzeRequest, User loginUser);
+
+    void checkSpaceAuth(User loginUser, Space space);
+}
